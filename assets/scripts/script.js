2a83c88692fc3fa5217693b9db2b117f221fbcc8
rash <orashusedmund@gmail.com>
2023-06-25 12:57:12 +0100

    done with the reset function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,3 +11,12 @@ const nickel = document.getElementById('nickel')
 const penny = document.getElementById('penny')
 const form = document.getElementById('cash-register')
 const resetBtn = document.getElementById('reset-btn')
+
+resetBtn.addEventListener('click', function () {
+  reload()
+})
+
+function reload () {
+  window.location.reload()
+}
+
